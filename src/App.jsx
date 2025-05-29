faa04786fb63017596c10f60f62772c018ef16ed
ravisquixta <ravi.shankar@quixta.in>
2025-05-29 21:17:15 +0530

    Refactor App component to update layout and styling; replace existing ReminderCard instances with multiple instances featuring specific props for improved clarity and organization of goals.

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,57 @@ import BottomNav from './components/BottomNav';
 
 const App = () => {
   return (
-    <div className="app">
+    <div className="bg-[#f3f4f6]">
       <Header />
       <main className="p-4">
-        <div className="flex items-center text-sm font-medium text-gray-700 mb-2">
-          <img src="/streakIcon.png" alt="Streak icon" className="h-4 w-4 mr-1" />
+        <div className="flex items-center text-sm font-medium text-[#848586] mb-[10px] border border-red-500">
+          <img src="/zap.svg" alt="Streak icon" className="h-4 w-4 mr-1" />
           your streaks
         </div>
         <StreakTracker />
         <TimeOfDayFilter />
-        <ReminderCard /> 
-        <ReminderCard />
-        <h2>Pending Goals</h2> 
-        <h2>Completed Goals</h2>
-     <GoalItem />
+        
+        <ReminderCard 
+          title="Morning Walk" 
+          pet="For Browny" 
+          time="2:00pm" 
+          frequency="Everyday" 
+        />
+        <ReminderCard 
+          title="Evening Walk" 
+          pet="For Browny" 
+          time="2:00pm" 
+          frequency="Everyday" 
+        />
+        <ReminderCard 
+          title="Breakfast" 
+          pet="For Browny" 
+          time="2:00pm" 
+          frequency="Everyday" 
+        />
+        <ReminderCard 
+          title="Lunch" 
+          pet="For Browny" 
+          time="2:00pm" 
+          frequency="Everyday" 
+        />
+        
+        <div className="mt-6 mb-2 text-gray-700 font-medium">
+          pending goals
+        </div>
+        
+        <ReminderCard 
+          title="Vet visit" 
+          pet="For Browny" 
+          time="2:00pm" 
+          frequency="Everyday" 
+        />
+        
+        <div className="mt-6 mb-2 text-gray-700 font-medium">
+          completed goals
+        </div>
+        
+        <GoalItem title="morning walk" />
       </main>
       <BottomNav />
     </div>
