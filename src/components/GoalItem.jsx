d09bf42dd55efd534273b361a10661318e19d37c
ravisquixta <ravi.shankar@quixta.in>
2025-05-29 21:16:40 +0530

    Refactor GoalItem component to accept a title prop and update styling for improved layout

--- src/components/GoalItem.jsx
+++ src/components/GoalItem.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const GoalItem = () => {
+const GoalItem = ({ 
+  title = "morning walk",
+}) => {
   return (
-    <div>
-   
+    <div className="bg-gray-100 rounded-xl p-3 mb-3 flex items-center justify-between">
+      <span className="text-gray-700">{title}</span>
+     
     </div>
   );
 };
